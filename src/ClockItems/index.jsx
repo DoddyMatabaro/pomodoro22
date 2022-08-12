601ea7fc839cab4d1fa3061adba4059aaffd8608
Doddy MATABARO <matabarododdy@gmail.com>
2022-08-12 13:29:06 +0200

    alarm audio player

--- src/ClockItems/index.jsx
+++ src/ClockItems/index.jsx
@@ -4,11 +4,13 @@ import  Option  from '../Option';
 import ShowClock from '../ShowClock';
 import Buttons from '../Buttons';
 import { useCountdownTimer } from 'use-countdown-timer';
-
+import UIFx from 'uifx'
+import bip from '../assets/kichen1.wav';
 
 function ClockItems() { 
-  const [breakNumber, setBreakNumber] = useState(0);
+  const [breakNumber, setBreakNumber] = useState(5);
   const [sessionTime, setSessionTime] = useState(25);
+  const [itBreak, setItBreak] = useState(false);
   const increase = (fnx, value, isBreak ) =>{ 
         return (value >= 0 && value < 60) ? 
             ((isBreak && value < 60)? fnx(value+1) : fnx(value+1) ) : null; 
@@ -17,21 +19,31 @@ function ClockItems() {
   const decrease = (fnx, value)=>{
         return  (value > 0  ) ? fnx(value-1) : null;
   }
-
+  
+      const alarm = new UIFx(
+        bip,{
+            volume: 0.8,
+            throttleMs: 100
+        }
+    )
     const { countdown, start, reset, pause, isRunning } = useCountdownTimer({
-      timer: 1000 * 60 * sessionTime,
+      timer: 1000 * 60 * (itBreak ? breakNumber :sessionTime ),
+      onExpire:()=>{   
+          setItBreak(!itBreak); 
+          reset();     
+          start();
+      },
     });
-
-    const decreaseCount = ()=>{
-
-    }
+  
     useEffect(() => {
-           
+        if(countdown<=10){
+          alarm.play();
+        } 
     });
   return (
     <article>
            <div className='options_container'>
-                <Option 
+                      <Option 
                         value={breakNumber} 
                         title="Break Length"                        
                         actions={[decrease, increase]}
